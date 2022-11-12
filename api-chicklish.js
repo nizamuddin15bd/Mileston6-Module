2d02c4daec5715fe82fd7b384aa853b166ed3c47
nizamuddin15bd <nizamuddin15bd@gmail.com>
2022-11-12 15:07:56 +0600

    end

---
+++ api-chicklish.js
@@ -0,0 +1,18 @@
+/*
+1. object with property (sting, boolean, number, object, array, mehtod wich will return a property value)
+2. template string. minimum 3  property will be dynamic(nested object property, array second element, another property)
+3. 1. no paarameter arrow function. return 89
+3. 2: single parameter arrow function. will divide input by 7
+3.3: arrow function with two parameters.
+3.4: mult line arrow funtion. two parameters. 
+.
+4. map divide each element by 7
+5. map, forEach, filter, find
+6. destructuring on object and array 
+.
+7.the cocktailDB.com
+8.forEach 
+9.jsonplaceHolder thakha img gulo dakhata hoba
+10. ==, ===, dakta hoba
+
+*/
